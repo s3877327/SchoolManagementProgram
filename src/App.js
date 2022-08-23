af640449d89cf42e5775689d7be0c1d21fa6d606
s3877327 <s3877327@rmit.edu.vn>
2022-08-23 10:20:43 +0700

    web merges
    web merges (Quang's part is still under work)

--- src/App.js
+++ src/App.js
@@ -1,26 +1,3 @@
-import logo from './logo.svg';
-import './App.css';
-<<<<<<< Updated upstream
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-=======
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import NavBar from './pages/NavBar';
 import AddStudentPage from './pages/AddStudentPage';
@@ -49,7 +26,6 @@ function App() {
       </Routes>
     </BrowserRouter>
     </>
->>>>>>> Stashed changes
   );
 }
 
