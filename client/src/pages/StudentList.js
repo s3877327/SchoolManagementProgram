65b7e0914a703515b78e48144d702476fee08fec
Minh Thong <s3852882@rmit.edu.vn>
2022-08-29 17:24:01 +0700

    finish display and test search function

--- client/src/pages/StudentList.js
+++ client/src/pages/StudentList.js
@@ -1,22 +1,24 @@
 import React, { Fragment, useState, useEffect } from "react";
 import { useSelector } from "react-redux";
-import {Link} from 'react-router-dom'
-import  StudentCard from '../components/StudentCard'
-import {allStudents} from '../action/student'
+import { Link } from "react-router-dom";
+import StudentCard from "../components/StudentCard";
+import { allStudents } from "../action/student";
 
 function StudentList() {
-const [student, setStudent] =useState([]);
-const {auth} = useSelector((state)=>({...state}));
-const{token} = auth;
+  const [student, setStudent] = useState([]);
+  const [query, setQuery] = useState(""); 
+  console.log(query);
+  const { auth } = useSelector((state) => ({ ...state }));
+  const { token } = auth;
 
-useEffect(()=>{
-  loadStudents()
-},[]);
+  useEffect(() => {
+    loadStudents();
+  }, []);
 
-const loadStudents = async()=>{
-  let res = await allStudents(auth.token);
-  setStudent(res.data);
-}
+  const loadStudents = async () => {
+    let res = await allStudents(auth.token);
+    setStudent(res.data);
+  };
 
   return (
     <Fragment>
@@ -25,26 +27,30 @@ const loadStudents = async()=>{
       </div>
 
       <div className="container-fluid m-3">
-        <div className="row justify-content-end m-5">
-          <div className="col-md-3">
+        <div className="row justify-content-start m-1">
             <div className="input-group">
-              <input type="text" className="form-control" />
-              <button className="btn btn-primary" type="button">
+              <input
+                type="text"
+                className="form-control"
+                placeholder="Search..."
+                onChange={(e)=>setQuery(e.target.value)}
+              />
+              
+              {student.filter(h=>h.studentName.toLowerCase().includes(query)).map((h)=>(
+                <div className="container-fluid">
+                  <br />
+                  <StudentCard key={h._id} h={h}/>
+                </div>
+              ))}
+              
+              {/* <button className="btn btn-primary" type="button">
                 Search
-              </button>
+              </button> */}
             </div>
-          </div>
-        
+          
         </div>
       </div>
-      <div className="container-fluid">
-        <br />
-        {/* <pre>{JSON.stringify(student, null, 4)}</pre> */}
-        {student.length > 0 &&
-          student.map((h) => (
-            <StudentCard key={h._id} h={h} />
-          ))}
-      </div>
+      
     </Fragment>
   );
 }
